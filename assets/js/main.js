87df46844dfe3a261409937e1e67bc6adddfc194
karimcode379 <karimcode379@gmail.com>
2022-05-17 11:51:47 +0200

    add changeInput

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 
+let changeInput1 = () => {
+    let label2 = document.getElementById('label2');
+    let person2 = document.getElementById('person2');
+
+    label2.style.display = 'none';
+    person2.style.display = 'none';
+}
+
+let changeInput2 = () => {
+    let label2 = document.getElementById('label2');
+    let person2 = document.getElementById('person2');
+
+    label2.style.display = 'block';
+    person2.style.display = 'block';
+}
+
